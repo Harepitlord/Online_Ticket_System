c4dfef52137ee6082e43f6d7c40167e8459c09d7
ilhamkhan <ilhampersonal412@mailcom>
2020-11-17 19:09:10 +0530

    updated page

---
+++ src/webpage/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="mystyles.css"/>
+    <script src="myscript.js"></script>
+</head>
+<body>
+  <div id="head">
+    <h1 id="he1"> Online Ticket Reservation System</h1>
+    <h3 id="he1">Software Engineering Laboratory <br></h3>
+    <h2 id="he1"> Login</h2>
+  </div>
+<form action="action_page.php" method="post">
+<div id="form_page">
+  <div id="form_body">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required><br>
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required><br>
+    <button type="submit">Login</button><br>
+    <label>
+  </div>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
+</body>
+</html>

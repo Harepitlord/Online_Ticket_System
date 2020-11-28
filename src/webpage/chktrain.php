139dbc9a3c3b289d1d460445314ad9e1c590d539
Hareeswaran O S <65818462+Harepitlord@users.noreply.github.com>
2020-11-28 13:07:02 +0530

    index page added

--- src/webpage/chktrain.php
+++ src/webpage/chktrain.php
@@ -7,25 +7,57 @@
     <script src="myscript.js"></script>
 </head>
 <body>
-  <div id="form_page">
-    <h1 id="he1"> Online Ticket Reservation System</h1>
+  <div style=" margin: auto; ">
+    <h1 id="he1"> Online Ticket Reservation System<br></h1>
     <h3 id="he1">Software Engineering Laboratory <br></h3>
     <h2 id="he1"> Check Train</h2>
   </div>
-  <div>
-    <form id="form_body" method="post">
-      <label for ="station">From:</label>
+  <div id="form_body" action="train.php">
+    <form  style=" height:40px;" method="post">
+      <label for ="From:">From:</label>
         <?php
           require_once "sqlconnect.php";
           $sql = "SELECT * FROM station";
           $stmt = $pdo->prepare($sql);
           $stmt->execute();
           $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-          echo "<select name='frst' id='fromst'>"
+          echo "<select name='frst' id='fromst'>\n";
+          echo "<option value='0'>Choose an option:</option>";
           foreach($rows as $row) {
-            echo "<option value=".$row['station_id'].'>'.$row['title'].'-'.$row['st_code'].'</option>';
+            echo ("<option value=".$row['station_id'].'>'.$row['title'].'-'.$row['st_code'].'</option>');
           }
-          ?>
-
+          echo "</select>";
+        ?>
+    </form>
+    <form  style="height:40px;" method="post">
+      <label for = 'To:'> To:</label>
+      <?php
+        echo "<select name='tost'>\n";
+        echo "<option value='0'>Choose an option:</option>";
+        foreach($rows as $row) {
+          if($row['station_id']== $_POST['frst'])
+            continue;
+          echo ("<option value=".$row['station_id'].'>'.$row['title'].'-'.$row['st_code'].'</option>');
+        }
+        echo "</select>";
+      ?>
+    </form>
+    <form style="height:40px;" method="post">
+      <label for='Day:'>Day:</label>
+      <?php
+      $sql = "SELECT * FROM day";
+      $stmt = $pdo->prepare($sql);
+      $stmt->execute();
+      $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      echo "<select name='day' id='dayselect'>\n";
+      echo "<option value='0'>Choose an option:</option>";
+      foreach($rows as $row) {
+        echo ("<option value=".$row['day_id'].'>'.$row['title'].'</option>');
+      }
+      echo "</select>";
+      ?>
+    <br><br>
+    <input type='submit' value='Submit'/>
+  </div>
 </body>
 </html>
--- src/webpage/sqlconnect.php
+++ src/webpage/sqlconnect.php
@@ -1,5 +1,5 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;port=3308;dbname=Online_Ticket',
-   'Hari', 'selabwork*2020');
+   'Hari', 'hari');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
